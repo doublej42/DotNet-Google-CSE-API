0d07672078067c2463d9ce62fe927431ad5dbb0a
Jeff Jacob <jeff.jacob@nanaimo.ca>
2014-01-31 15:04:14 -0800

    Ready to build nuget package

---
+++ GoogleCSE/SearchUnitTest/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using GoogleCSE;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SearchUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var gs = new GoogleSearch(TestConfigs.Cseid, maxPages: 2); 
+            var results = gs.Search("dam");
+            Assert.IsTrue(results.Any());
+            Assert.IsTrue(results.Count <= 40);
+            results = gs.Search("dam","Businesses");
+            Assert.IsTrue(results.Count == 2);
+        }
+    }
+}
