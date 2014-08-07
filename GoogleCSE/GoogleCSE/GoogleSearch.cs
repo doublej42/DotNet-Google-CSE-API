41bb7dfbadcf752b0e8bfe48fd163a9c88cd9d14
Jeff Jacob <jeff.jacob@nanaimo.ca>
2014-08-07 16:25:33 -0700

    CSE Interface Working

--- GoogleCSE/GoogleCSE/GoogleSearch.cs
+++ GoogleCSE/GoogleCSE/GoogleSearch.cs
@@ -48,7 +48,7 @@ namespace GoogleCSE
         /// </summary>
         public Dictionary<string, string> Labels { get; set; }
         /// <summary>
-        /// total results, all these results might not actually be available.
+        /// Total results, all these results might not actually be available.
         /// </summary>
         public long TotalResults { get; set; }
         /// <summary>
@@ -57,18 +57,17 @@ namespace GoogleCSE
         public double SearchTime { get; set; }
     }
 
-    //public static class GoogleApiMethod
-    //{
-    //    public static string XML = "XML";
-    //}
 
 // ReSharper disable InconsistentNaming
+    /// <summary>
+    /// The two api's used for searching google. The CSE is the default. See GoogleSearch constructor for more information.
+    /// </summary>
     public enum GoogleSearchMethod { XML, CSE};
 // ReSharper restore InconsistentNaming
 
     /// <summary>
     /// Class to instantiate for searching a Google custom search engine. You will need to provide your cx
-    /// https://developers.google.com/custom-search/docs/xml_results?hl=en&csw=1
+    /// XML Docs: https://developers.google.com/custom-search/docs/xml_results
     /// You can get your CSE from https://www.google.com/cse/
     /// </summary>
     public class GoogleSearch
@@ -95,12 +94,12 @@ namespace GoogleCSE
         /// <param name="extraOptions">(optional)If you need to supply more custom options passed to google please include them here. These options will override any defaults</param>
         /// <param name="pageSize">(optional) Number of results per page, Default are 10 but there is a max of 20 with the XMl API</param>
         /// <param name="maxPages">(optional) Number of pages to grab. Larger number will be slower.  Also each page is a new search on your paid search limit.</param>
-        /// <param name="start">The result offset, use to page through results.</param>
-        /// <param name="method">(optional) API Used to gather the results. Default is XML https://developers.google.com/custom-search/docs/xml_results the other option is CSE https://developers.google.com/custom-search/json-api/v1/reference/cse/list 
-        /// Only the CSA interface gives promotions as a separate object
+        /// <param name="start">The result offset, use to page through results. WARNING: CSE starts at 1 but XML starts at zero. If you want the 11th result (page 2) on CSE set this to 11. On XML set this to 10 </param>
+        /// <param name="method">(optional) API Used to gather the results. Default isCSE https://developers.google.com/custom-search/json-api/v1/reference/cse/list the other option is  XML https://developers.google.com/custom-search/docs/xml_results 
+        /// Only the CSA interface gives promotions as a separate object. Not this is a change of default from old versions.
         /// </param>
         /// <param name="key">Api Key, only needed if you are using the CSE interface</param>
-        public GoogleSearch(string cx, string hl = "en", Dictionary<string, string> extraOptions = null, int pageSize = 10, int maxPages = 20, int start = 1, GoogleSearchMethod method = GoogleSearchMethod.XML, string key = "")
+        public GoogleSearch(string cx, string hl = "en", Dictionary<string, string> extraOptions = null, int pageSize = 10, int maxPages = 20, int start = 1, GoogleSearchMethod method = GoogleSearchMethod.CSE, string key = "")
         {
             
             Options["client"] = "google-csbe";
@@ -126,9 +125,9 @@ namespace GoogleCSE
         }
 
         /// <summary>
-        /// build a query url
+        /// Build a query URL
         /// </summary>
-        /// <returns></returns>
+        /// <returns>string: The URL used by the various search functions</returns>
         protected string QueryUrl()
         {
             var sb = new StringBuilder();
@@ -159,9 +158,9 @@ namespace GoogleCSE
         /// <summary>
         /// Get the search results 
         /// </summary>
-        /// <param name="term">term to search</param>
-        /// <param name="label">If you are using custom refinements this will help you search part of your site.</param>
-        /// <returns></returns>
+        /// <param name="term">Term to search</param>
+        /// <param name="label">If you are using custom refinements this will help you search part of your site</param>
+        /// <returns>List of Search Results</returns>
         public List<GoogleSearchResult> Search(string term, string label = "")
         {
 
@@ -177,6 +176,12 @@ namespace GoogleCSE
             return RecursiveResults(url);
         }
 
+        /// <summary>
+        /// Searches Google and returns a Google search results object. This includes details about refinements (labels) and total results, time to search (CSE only) and promotional results.
+        /// </summary>
+        /// <param name="term">Term to search</param>
+        /// <param name="label">If you are using custom refinements this will help you search part of your site</param>
+        /// <returns>GoogleSearchResults object. See object for more details</returns>
         public GoogleSearchResults SearchDetailed(string term, string label = "")
         {
             if (!string.IsNullOrWhiteSpace(label))
@@ -191,7 +196,12 @@ namespace GoogleCSE
             return RecursiveResultDetailed(url);
         }
 
-        private GoogleSearchResults RecursiveResultDetailed(string url, int depth = 1)
+        /// <summary>
+        /// Used to search multiple pages and combine them with details
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        private GoogleSearchResults RecursiveResultDetailed(string url)
         {
             var ret = new GoogleSearchResults
             {
@@ -212,8 +222,7 @@ namespace GoogleCSE
                 var nsOpenSearch = data.GetNamespaceOfPrefix("opensearch");
                 var nsCse = data.GetNamespaceOfPrefix("cse");
                 var nsAtom = data.GetDefaultNamespace();
-                if (depth == 1) // only check this on the first result
-                {
+              
                     //get the search information
                     var searchInformation = data.Element(nsCse + "searchInformation");
                     if (searchInformation != null)
@@ -225,32 +234,201 @@ namespace GoogleCSE
                         if (searchTime != null)
                             ret.SearchTime = double.Parse(searchTime.Value);
                     }
-                    //TODO Get labels
-
-                    //TODO Get Promotions
-                }
+                    //Get labels
+                    foreach (var label in data.Descendants(nsCse + "facet"))
+                    {
+                        var item = label.Element(nsCse + "item");
+                        if (item != null)
+                        {
+                            var key = item.Attribute("label").Value;
+                            var value = item.Attribute("anchor").Value;
+                            ret.Labels[key] = value;
+                        }
+                    }
+                    //Get Promotions
+                    foreach (var promotion in data.Descendants(nsCse + "promotion"))
+                    {
+                        var tmpResult = new GoogleSearchResult();
+                        var title = promotion.Element(nsAtom + "title");
+                        if (title != null) tmpResult.Title = title.Value;
+                        var link = promotion.Element(nsAtom + "link");
+                        if (link != null)
+                        {
+                            if (link.Attribute("href") != null)
+                            {
+                                tmpResult.Url = link.Attribute("href").Value;
+                            }
+                        }
+                        var description = promotion.Element(nsCse + "bodyLine");
+                        if (description != null)
+                        {
+                            tmpResult.Description = description.Attribute("title").Value;
+                        }
+                        ret.Promotions.Add(tmpResult);
+                    }
+                
                 //get search Results
                 //foreach (var searchResult in data.Descendants(nsAtom+"entry"))
                 //{
                 //    ret.Results.Add(ParseCseResult(searchResult));
                 //}
                 ret.Results.AddRange(data.Descendants(nsAtom + "entry").Select(ParseCseResult));
-                if (depth < _maxPages)
+                if (1 < _maxPages)
                 {
                     var nextPage = data.Elements(nsOpenSearch + "Query").FirstOrDefault(e => e.Attribute("role").Value == "cse:nextPage");
                     if (nextPage != null)
                     {
+                        string oldstart = null;
+                        if (Options.ContainsKey("start"))
+                        { 
+                            oldstart = Options["start"];
+                        }
                         Options["start"] = nextPage.Attribute("startIndex").Value;
                         var nextUrl = QueryUrl();
-                        var theRestOfThePages = RecursiveResultDetailed(nextUrl, ++depth);
-                        ret.Results.AddRange(theRestOfThePages.Results);
+                        var theRestOfThePages = RecursiveResults(nextUrl, 2);
+                        ret.Results.AddRange(theRestOfThePages);
+                        if (oldstart != null)
+                        {
+                            Options["start"] = oldstart;
+                        }
+                        else
+                        {
+                            Options.Remove("start");
+                        }
                     }
                 }
             }
             if (Method == GoogleSearchMethod.XML)
             {
-                //TODO
+                var xResults = XDocument.Load(url);
+                //get labels
+                foreach (var label in xResults.Descendants("FacetItem"))
+                {
+                    var key = label.Element("label");
+                    if (key != null)
+                    {
+                        var lblDesc = label.Element("anchor_text");
+                        if (lblDesc != null)
+                        {
+                            ret.Labels[key.Value] = lblDesc.Value;
+                        }
+                    }
+                }
+
+
+                var data = xResults.Descendants("RES").FirstOrDefault();
+                if (data == null) return ret;
+                var totalResults = data.Element("M");
+                if (totalResults != null)
+                {
+                    ret.TotalResults = long.Parse(totalResults.Value);
+                }
+                //ret.Results.AddRange(data.Descendants("R").Select(ParseXmlResult));
+                foreach (var r in data.Descendants("R"))
+                {
+                    var tmpResults = ParseXmlResult(r);
+                    var slR = r.Element("SL_RESULTS");
+                    if (slR != null)
+                    {
+                        //promotion
+                        var bodyLine = slR.Descendants("BODY_LINE").FirstOrDefault();
+                        if (bodyLine != null)
+                        {
+                            var t = bodyLine.Descendants("T").FirstOrDefault();
+                            if (t != null) tmpResults.Description = t.Value;
+                            ret.Promotions.Add(tmpResults);
+                        }
+                        
+                    }
+                    else
+                    {
+                        ret.Results.Add(tmpResults);
+                    }
+                }
+                var nb = data.Element("NB");
+                if (nb == null) return ret;
+                var nu = nb.Element("NU");
+                if (nu == null) return ret;
+                if (1 < _maxPages)
+                {
+                    ret.Results.AddRange(RecursiveResults("http://www.google.com" + nu.Value,2));
+                }
             }
+            return ret;
+        }
+
+
+
+        /// <summary>
+        /// Walk through pages of results
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="depth"></param>
+        /// <returns></returns>
+        private List<GoogleSearchResult> RecursiveResults(string url, int depth = 1)
+        {
+            var ret = new List<GoogleSearchResult>();
+            if (Method == GoogleSearchMethod.XML)
+            {
+                var xResults = XDocument.Load(url);
+                var data = xResults.Descendants("RES").FirstOrDefault();
+                if (data == null) return ret;
+                foreach (var r in data.Descendants("R"))
+                {
+                    var tmpResults = ParseXmlResult(r);
+                    var slR = r.Element("SL_RESULTS");
+                    if (slR == null) // ignore promotions
+                    {
+                        ret.Add(tmpResults);
+                    }
+                }
+                var nb = data.Element("NB");
+                if (nb == null) return ret;
+                var nu = nb.Element("NU");
+                if (nu == null) return ret;
+                if (depth < _maxPages)
+                {
+                    ret.AddRange(RecursiveResults("http://www.google.com" + nu.Value, ++depth));
+                }
+            }
+            if (Method == GoogleSearchMethod.CSE)
+            {
+                
+                var xResults = XDocument.Load(url);
+                var data = xResults.Root;
+                if (data == null)
+                {
+                    return ret;
+                }
+                var nsOpenSearch = data.GetNamespaceOfPrefix("opensearch");
+                var nsAtom = data.GetDefaultNamespace();
+                ret.AddRange(data.Descendants(nsAtom + "entry").Select(ParseCseResult));
+                if (depth < _maxPages)
+                {
+                    var nextPage = data.Elements(nsOpenSearch + "Query").FirstOrDefault(e => e.Attribute("role").Value == "cse:nextPage");
+                    if (nextPage != null)
+                    {
+                        string oldstart = null;
+                        if (Options.ContainsKey("start"))
+                        {
+                            oldstart = Options["start"];
+                        }
+                        Options["start"] = nextPage.Attribute("startIndex").Value;
+                        var nextUrl = QueryUrl();
+                        var theRestOfThePages = RecursiveResults(nextUrl, ++depth);
+                        ret.AddRange(theRestOfThePages);
+                        if (oldstart != null)
+                        {
+                            Options["start"] = oldstart;
+                        }
+                        else
+                        {
+                            Options.Remove("start");
+                        }
+                    }
+                }
+            }
+
             return ret;
         }
 
@@ -296,57 +474,6 @@ namespace GoogleCSE
             var xElement1 = r.Element("S");// won't work for promoted results but they are just shown inline with normal results
             if (xElement1 != null) tmpR.Description = xElement1.Value;
             return tmpR;
-        }
-
-        /// <summary>
-        /// Walk through pages of results
-        /// </summary>
-        /// <param name="url"></param>
-        /// <param name="depth"></param>
-        /// <returns></returns>
-        private List<GoogleSearchResult> RecursiveResults(string url, int depth = 1)
-        {
-            var ret = new List<GoogleSearchResult>();
-            if (Method == GoogleSearchMethod.XML)
-            {
-                var xResults = XDocument.Load(url);
-                var data = xResults.Descendants("RES").FirstOrDefault();
-                if (data == null) return ret;
-                ret.AddRange(data.Descendants("R").Select(ParseXmlResult));
-                var nb = data.Element("NB");
-                if (nb == null) return ret;
-                var nu = nb.Element("NU");
-                if (nu == null) return ret;
-                if (depth < _maxPages)
-                {
-                    ret.AddRange(RecursiveResults("http://www.google.com" + nu.Value, ++depth));
-                }
-            }
-            if (Method == GoogleSearchMethod.CSE)
-            {
-                
-                var xResults = XDocument.Load(url);
-                var data = xResults.Root;
-                if (data == null)
-                {
-                    return ret;
-                }
-                var nsOpenSearch = data.GetNamespaceOfPrefix("opensearch");
-                var nsAtom = data.GetDefaultNamespace();
-                ret.AddRange(data.Descendants(nsAtom + "entry").Select(ParseCseResult));
-                if (depth < _maxPages)
-                {
-                    var nextPage = data.Elements(nsOpenSearch + "Query").FirstOrDefault(e => e.Attribute("role").Value == "cse:nextPage");
-                    if (nextPage != null)
-                    {
-                        Options["start"] = nextPage.Attribute("startIndex").Value;
-                        var nextUrl = QueryUrl();
-                        ret.AddRange(RecursiveResults(nextUrl, ++depth));
-                    }
-                }
-            }
-
-            return ret;
         }
 
 
--- GoogleCSE/SearchUnitTest/UnitTest1.cs
+++ GoogleCSE/SearchUnitTest/UnitTest1.cs
@@ -11,8 +11,10 @@ namespace SearchUnitTest
         [TestMethod]
         public void TestMethodSimpleXml()
         {
-            var gs = new GoogleSearch(TestConfigs.Cseid, maxPages: 2, pageSize: 20); 
-            var results = gs.Search("dam");
+            var gs = new GoogleSearch(TestConfigs.Cseid, maxPages: 2, pageSize: 20, method: GoogleSearchMethod.XML);
+            Assert.IsFalse(gs.Options.ContainsKey("start"));
+            var results = gs.Search("garbage");
+            Assert.IsFalse(gs.Options.ContainsKey("start"));
             Assert.IsTrue(results.Any());
             Assert.IsTrue(results.Count <= 40);
             Assert.IsTrue(results.Count > 30);
@@ -21,32 +23,41 @@ namespace SearchUnitTest
         }
 
         [TestMethod]
-        public void TestMethodCseDetails()
-        {
-            var gs = new GoogleSearch(TestConfigs.Cseid, maxPages: 2,key: TestConfigs.Key, method: GoogleSearchMethod.CSE );
-            var results = gs.SearchDetailed("garbage");
-
-            Assert.IsTrue(results.Results.Any());
-            Assert.IsTrue(results.Results.Count <= 20);
-            Assert.IsTrue(results.Results.Count > 10);
-            gs.Options["start"] = "11";
-            var results2 = gs.SearchDetailed("garbage");
-            Assert.AreEqual(results.Results[10].Url,results2.Results[0].Url);
-            results = gs.SearchDetailed("dam", "Businesses");
-            Assert.IsTrue(results.Results.Count == results.TotalResults);
-        }
-
-        [TestMethod]
         public void TestSimpleCse()
         {
             var gs = new GoogleSearch(TestConfigs.Cseid, maxPages: 2, pageSize: 10, key: TestConfigs.Key, method: GoogleSearchMethod.CSE);
+            Assert.IsFalse(gs.Options.ContainsKey("start"));
             var results = gs.Search("dam");
+            Assert.IsFalse(gs.Options.ContainsKey("start"));
             Assert.IsTrue(results.Any());
             Assert.IsTrue(results.Count <= 20);
             Assert.IsTrue(results.Count > 10);
             results = gs.Search("dam", "Businesses");
             Assert.IsTrue(results.Count == 2);
         }
+
+        [TestMethod]
+        public void TestMethodCseDetails()
+        {
+            var gs = new GoogleSearch(TestConfigs.Cseid, maxPages: 2,key: TestConfigs.Key, method: GoogleSearchMethod.CSE );
+            Assert.IsFalse(gs.Options.ContainsKey("start"));
+            var results = gs.SearchDetailed("garbage");
+            Assert.IsFalse(gs.Options.ContainsKey("start"));
+
+            Assert.IsTrue(results.Results.Any());
+            Assert.IsTrue(results.Results.Count <= 20);
+            Assert.IsTrue(results.Results.Count > 10);
+            Assert.IsTrue(results.Promotions.Any(r => r.Description.Contains("garbage")));
+            Assert.IsTrue(results.Labels.ContainsKey("art"));
+            gs.Options["start"] = "11";
+            var results2 = gs.SearchDetailed("garbage");
+            Assert.IsTrue(gs.Options["start"] == "11");
+            Assert.AreEqual(results.Results[10].Url,results2.Results[0].Url);
+            results = gs.SearchDetailed("dam", "Businesses");
+            Assert.IsTrue(results.Results.Count == results.TotalResults);
+        }
+
+       
 
         [TestMethod]
         public void TestXmlDetails()
@@ -57,8 +68,10 @@ namespace SearchUnitTest
             Assert.IsTrue(results.Results.Any());
             Assert.IsTrue(results.Results.Count <= 20);
             Assert.IsTrue(results.Results.Count > 10);
-            gs.Options["start"] = "11";
+            Assert.IsTrue(results.Labels.ContainsKey("art"));
+            gs.Options["start"] = "10";
             var results2 = gs.SearchDetailed("garbage");
+            Assert.IsTrue(gs.Options["start"] == "10");
             Assert.AreEqual(results.Results[10].Url, results2.Results[0].Url);
             results = gs.SearchDetailed("dam", "Businesses");
             Assert.IsTrue(results.Results.Count == results.TotalResults);
